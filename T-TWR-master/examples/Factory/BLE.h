4ed85d258cdbbe051ec7a6015fd19d86d99e6573
sascha-maibach <sascha.maibach@unibas.ch>
2025-01-28 13:41:42 +0100

    add debug print to new function

--- T-TWR-master/examples/Factory/BLE.h
+++ T-TWR-master/examples/Factory/BLE.h
@@ -44,7 +44,8 @@ public:
             size_t length = value.length();
             size_t i = 0;
             for (char c : value) {
-                    bitpack[i++] = static_cast<uint8_t>(c);
+                printf("char %d in values: %s", i, c);
+                bitpack[i++] = static_cast<uint8_t>(c);
             }
             
             Serial.print("nach copy");
