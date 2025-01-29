ade17540adbfb2ac16c232cbe993d12dc5455d46
AdrianProk <71382763+AdrianProk@users.noreply.github.com>
2025-01-29 10:17:34 +0100

    dauersender, delay entfernung

--- T-TWR-master/examples/Factory/Factory.ino
+++ T-TWR-master/examples/Factory/Factory.ino
@@ -78,6 +78,12 @@ void setup() {
 }
 
 void loop() {
+    if(!bitready) {
+        radio.transmit();
+        ledcWriteTone(0, 200);
+        ledcWriteTone(0, 1600);
+        radio.receive();
+    }
     if (bitready) {
             //uint8_t values_from_bluetooth_array[512];
             uint8_t* values_from_bluetooth_array = (uint8_t*) calloc(512, sizeof(uint8_t));
@@ -145,7 +151,7 @@ void playMessage(uint8_t pin, uint8_t channel, uint8_t message[])
         } else {
             ledcWriteTone(channel, 1600);
         }
-        delay(100);
+        //delay(100)
     }
     // ledcWriteTone(channel, 0);
     ledcDetachPin(pin);
